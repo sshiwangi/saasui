19b4b365e24d2b51b373700e949b724f1048e422
Shiwangi <sshiwangi770@gmail.com>
2024-11-12 14:37:24 +0530

    fixed footer import

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/shared/Navbar";
-import Footer from "@/components/ui/Footer";
+import Footer from "@/components/shared/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
